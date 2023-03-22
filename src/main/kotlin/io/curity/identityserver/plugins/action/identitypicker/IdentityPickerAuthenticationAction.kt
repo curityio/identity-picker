ce37ac62d2805890ec42c851198edfd526b63471
Daniel Lindau <d.lindau@gmail.com>
2023-03-22 15:59:20 +0100

    Use new SDK since the old one is deprecated

--- src/main/kotlin/io/curity/identityserver/plugins/action/identitypicker/IdentityPickerAuthenticationAction.kt
+++ src/main/kotlin/io/curity/identityserver/plugins/action/identitypicker/IdentityPickerAuthenticationAction.kt
@@ -19,61 +19,49 @@ package io.curity.identityserver.plugins.action.identitypicker
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import se.curity.identityserver.sdk.attribute.Attribute
-import se.curity.identityserver.sdk.attribute.AttributeValue
-import se.curity.identityserver.sdk.attribute.AuthenticationAttributes
-import se.curity.identityserver.sdk.attribute.ListAttributeValue
-import se.curity.identityserver.sdk.attribute.MapAttributeValue
-import se.curity.identityserver.sdk.attribute.SubjectAttributes
-import se.curity.identityserver.sdk.authentication.AuthenticatedSessions
+import se.curity.identityserver.sdk.attribute.*
 import se.curity.identityserver.sdk.authenticationaction.AuthenticationAction
+import se.curity.identityserver.sdk.authenticationaction.AuthenticationActionContext
 import se.curity.identityserver.sdk.authenticationaction.AuthenticationActionResult
 import se.curity.identityserver.sdk.authenticationaction.AuthenticationActionResult.failedResult
 import se.curity.identityserver.sdk.authenticationaction.AuthenticationActionResult.successfulResult
 import se.curity.identityserver.sdk.authenticationaction.completions.RequiredActionCompletion.PromptUser.prompt
-import se.curity.identityserver.sdk.service.authenticationaction.AuthenticatorDescriptor
 
 const val PICKED_IDENTITY_SESSION_KEY = "$PLUGIN_TYPE-identity"
 const val IDENTITY_LIST_SESSION_KEY = "$PLUGIN_TYPE-identity_list"
 
 class IdentityPickerAuthenticationAction(private val config: IdentityPickerAuthenticationActionConfig) :
-        AuthenticationAction
-{
-    companion object
-    {
+    AuthenticationAction {
+    companion object {
         private val logger: Logger = LoggerFactory.getLogger(IdentityPickerAuthenticationAction::class.java)
     }
 
-    override fun apply(authenticationAttributes: AuthenticationAttributes,
-                       authenticatedSessions: AuthenticatedSessions,
-                       authenticationTransactionId: String,
-                       authenticatorDescriptor: AuthenticatorDescriptor): AuthenticationActionResult
-    {
+    override fun apply(context: AuthenticationActionContext): AuthenticationActionResult {
 
         val sessionManager = config.sessionManager()
         val identity: Attribute? = sessionManager.get(PICKED_IDENTITY_SESSION_KEY)
+        val authenticationAttributes = context.authenticationAttributes
 
-        if (identity != null)
-        {
+        if (identity != null) {
             sessionManager.remove(PICKED_IDENTITY_SESSION_KEY)
             return pickIdentity(authenticationAttributes, identity.attributeValue)
         }
 
-        val identityList = authenticationAttributes.subjectAttributes[config.identityListAttribute()]?.attributeValue
-                as? ListAttributeValue
+        val identityList = authenticationAttributes
+            .subjectAttributes[config.identityListAttribute()]?.attributeValue as? ListAttributeValue
 
         logger.debug("Received list of identities: $identityList")
 
-        if (identityList == null)
-        {
-            logger.info("User with with subject '${authenticationAttributes.subject} did not have a list of " +
-                    "identities to chose from")
+        if (identityList == null) {
+            logger.info(
+                "User with with subject '${authenticationAttributes.subject} did not have a list of " +
+                        "identities to chose from"
+            )
             return failedResult("No account available")
         }
 
 
-        if (identityList.size() == 1)
-        {
+        if (identityList.size() == 1) {
             logger.debug("Choosing only available identity")
             return pickIdentity(authenticationAttributes, identityList.first())
         }
@@ -83,14 +71,16 @@ class IdentityPickerAuthenticationAction(private val config: IdentityPickerAuthe
         return AuthenticationActionResult.pendingResult(prompt())
     }
 
-    private fun pickIdentity(authenticationAttributes: AuthenticationAttributes,
-                             identity: AttributeValue): AuthenticationActionResult
-    {
+    private fun pickIdentity(
+        authenticationAttributes: AuthenticationAttributes,
+        identity: AttributeValue
+    ): AuthenticationActionResult {
         identity as? MapAttributeValue ?: throw IllegalArgumentException("Identity could not be parsed as a Map")
 
         val replacedAttributes = AuthenticationAttributes.of(
-                SubjectAttributes.of(authenticationAttributes.subject, identity),
-                authenticationAttributes.contextAttributes)
+            SubjectAttributes.of(authenticationAttributes.subject, identity),
+            authenticationAttributes.contextAttributes
+        )
 
         return successfulResult(replacedAttributes)
     }
