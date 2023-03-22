a1498fdcd78cf73455c06ed0f8ca9587d36e669d
Daniel Lindau <d.lindau@gmail.com>
2023-03-22 13:47:08 +0100

    Configure an attribute to display

--- src/main/kotlin/io/curity/identityserver/plugins/action/identitypicker/IdentityPickerRequestHandler.kt
+++ src/main/kotlin/io/curity/identityserver/plugins/action/identitypicker/IdentityPickerRequestHandler.kt
@@ -25,58 +25,60 @@ import se.curity.identityserver.sdk.attribute.ListAttributeValue
 import se.curity.identityserver.sdk.attribute.MapAttributeValue
 import se.curity.identityserver.sdk.authenticationaction.completions.ActionCompletionRequestHandler
 import se.curity.identityserver.sdk.authenticationaction.completions.ActionCompletionResult
+import se.curity.identityserver.sdk.errors.ErrorCode.INVALID_INPUT
 import se.curity.identityserver.sdk.service.SessionManager
 import se.curity.identityserver.sdk.web.Request
 import se.curity.identityserver.sdk.web.Response
+import se.curity.identityserver.sdk.web.Response.ResponseModelScope.ANY
 import se.curity.identityserver.sdk.web.ResponseModel.templateResponseModel
-import java.lang.RuntimeException
 import java.util.Optional
 
 private const val IDENTITIES_TEMPLATE_KEY = "_identities"
+private const val DISPLAY_NAME_ATTRIBUTE_NAME = "_displayNameAttribute"
 
-class IdentityPickerRequestHandler(private val sessionManager: SessionManager) :
-        ActionCompletionRequestHandler<IdentityPickerRequestModel>
-{
+class IdentityPickerRequestHandler(private val config: IdentityPickerAuthenticationActionConfig) :
+    ActionCompletionRequestHandler<IdentityPickerRequestModel> {
+
+    private val sessionManager = config.sessionManager()
+    private val exceptionFactory = config.exceptionFactory()
+
     override fun preProcess(request: Request, response: Response): IdentityPickerRequestModel =
-            IdentityPickerRequestModel(request, sessionManager)
+        IdentityPickerRequestModel(request, sessionManager)
 
     override fun get(request: IdentityPickerRequestModel, response: Response): Optional<ActionCompletionResult>
     {
-        response.setResponseModel(templateResponseModel(emptyMap(), "index"),
-                Response.ResponseModelScope.ANY)
+        response.setResponseModel(templateResponseModel(emptyMap(), "index"), ANY)
 
-        val model = convertAvailableIdentities(request.getIdentities())
-        response.putViewData(IDENTITIES_TEMPLATE_KEY, model, Response.ResponseModelScope
-                .ANY)
+        val identities = convertAvailableIdentities(request.getIdentities())
+        response.putViewData(IDENTITIES_TEMPLATE_KEY, identities, ANY)
+        response.putViewData(DISPLAY_NAME_ATTRIBUTE_NAME, config.displayNameAttribute(), ANY)
         return Optional.empty()
     }
 
-    override fun post(request: IdentityPickerRequestModel, response: Response): Optional<ActionCompletionResult>
-    {
+    override fun post(request: IdentityPickerRequestModel, response: Response): Optional<ActionCompletionResult> {
         val selectedIdentityIndex = request.getSelectedIdentityIndex()
         val modelFromSession = getAndRemoveModelFromSession()
-        val selectedIdentity = modelFromSession[selectedIdentityIndex] as? Map<*, *> ?: throw RuntimeException ("No matching identity selected")
+        val selectedIdentity = modelFromSession[selectedIdentityIndex] as? Map<*, *>
+                ?: throw exceptionFactory.badRequestException(INVALID_INPUT, "No matching identity selected")
 
         sessionManager.put(Attribute.of(PICKED_IDENTITY_SESSION_KEY, MapAttributeValue.of(selectedIdentity)))
         return Optional.of(ActionCompletionResult.complete())
     }
 
-    private fun getAndRemoveModelFromSession(): Map<String, Any>
-    {
+    private fun getAndRemoveModelFromSession(): Map<String, Any> {
         val model = sessionManager.remove("identity-model")?.attributeValue as? MapAttributeValue
-                ?: throw IllegalStateException("Did not find model in session")
+            ?: throw IllegalStateException("Did not find model in session")
         return model.value
     }
 
-    private fun convertAvailableIdentities(attribute: Attribute): Map<String, Map<String, Any>>
-    {
+    private fun convertAvailableIdentities(attribute: Attribute): Map<String, Map<String, Any>> {
         val listOfIdentities = attribute.attributeValue as? ListAttributeValue
-                ?: throw IllegalArgumentException("Identities was not in expected format")
+            ?: throw IllegalArgumentException("Identities was not in expected format")
 
         val identityModel = mutableMapOf<String, Map<String, Any>>()
         listOfIdentities.forEachIndexed { index, identityAttributeValue ->
             val identityAsMap = identityAttributeValue as? MapAttributeValue
-                    ?: throw IllegalArgumentException("Identity was not a map")
+                ?: throw IllegalArgumentException("Identity was not a map")
             identityModel[index.toString()] = identityAsMap.value
         }
 
@@ -86,9 +88,7 @@ class IdentityPickerRequestHandler(private val sessionManager: SessionManager) :
     }
 }
 
-class IdentityPickerRequestModel(request: Request,
-                                 sessionManager: SessionManager)
-{
+class IdentityPickerRequestModel(request: Request, sessionManager: SessionManager) {
 
     @Valid
     val getRequestModel = if (request.isGetRequest) GetRequestModel(sessionManager) else null
@@ -98,19 +98,17 @@ class IdentityPickerRequestModel(request: Request,
 
 
     fun getIdentities() = getRequestModel?.identities
-            ?: throw IllegalStateException("Request Model was missing for GET request")
+        ?: throw IllegalStateException("Request Model was missing for GET request")
 
     fun getSelectedIdentityIndex() = postRequestModel?.identityIndex
-            ?: throw IllegalStateException("Request Model was missing for POST request")
+        ?: throw IllegalStateException("Request Model was missing for POST request")
 
-    class PostRequestModel(request: Request)
-    {
+    class PostRequestModel(request: Request) {
         @NotBlank(message = "error.no.selected.identity")
         val identityIndex: String? = request.getFormParameterValues("identity-index").firstOrNull()
     }
 
-    class GetRequestModel(sessionManager: SessionManager)
-    {
+    class GetRequestModel(sessionManager: SessionManager) {
         @NotNull(message = "error.no.identities.in.session")
         val identities: Attribute = sessionManager[IDENTITY_LIST_SESSION_KEY]
     }
